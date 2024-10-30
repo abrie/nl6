e8e482f24e8fe5a641c0af228eb02caff2adc2d8
abrie <abrie@hello>
2024-10-30 11:32:20 -0400

    Changed Phaser input because it's broken for some reason...

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import Phaser from 'phaser';
+import * as Phaser from "phaser";
 
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	scene: {
+		preload: preload,
+		create: create,
+		update: update,
+	},
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  // Load assets here
+	// Load assets here
 }
 
 function create() {
-  // Create game objects here
+	// Create game objects here
 }
 
 function update() {
-  // Update game objects here
+	// Update game objects here
 }
